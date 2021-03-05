189910a7225bab0ac8fa0c093d6627f5d7283ee0
Yousef Ballan <ballan_yousef@yahoo.com>
2021-03-05 10:44:08 +0200

    Initial commit

---
+++ app/src/test/java/com/it/yb/mydbcontactapp/Point2dTest.java
@@ -0,0 +1,19 @@
+package com.it.yb.mydbcontactapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Point2dTest {
+    @Test
+    public void toDo1(){
+        Point2d p=new Point2d(5,7);
+
+    }
+    @Test
+    public void toDo2(){
+        Point2d p=new Point2d(5,7);
+
+    }
+
+}
